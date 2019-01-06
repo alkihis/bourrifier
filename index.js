b59cbdf0b4fad3c4d67a77de05cbd2fda6df6eeb
Louis Alkihis <alkihis@gmail.com>
2019-01-06 12:45:58 +0100

    uhefiusb

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const one_week = 60*60*24*7;
 
 for (const f of files_of_dir) {
     if (fs.statSync(f).mtime < (current_timestamp - one_week)) {
-        fs.unlink(f);
+        fs.unlink(f, () => {});
     }
 }
 
